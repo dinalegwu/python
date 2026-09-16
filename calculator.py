c7070a8d54b5d83dabc709b19de8ca1a37a98e75
dinalegwu <inalegwudorcas25@gmail.com>
2026-09-16 15:24:40 +0100

    calculator 2

--- calculator.py
+++ calculator.py
@@ -1,22 +1,33 @@
-firstdig = int(input())
-operator = input()
-secondig = int(input(""))
-equal_to = input()
-final = ""
+running = True
 
-if equal_to != "=":
-    final = "Come Again :)"
-else:
-    if operator == "+":
-        final = firstdig + secondig
-    elif operator == "-":
-        final = firstdig - secondig
-    elif operator == "*":
-        final = firstdig * secondig
-    elif operator == "/":
-        final = firstdig / secondig
+while running:
+    firstdig = int(input())
+    operator = input()
+    secondig = int(input(""))
+    equal_to = input()
+    final = ""
+
+    if equal_to != "=":
+        final = "Come Again :)"
     else:
-        print("Unknown operator")
+        if operator == "+":
+            final = firstdig + secondig
+        elif operator == "-":
+            final = firstdig - secondig
+        elif operator == "*":
+            final = firstdig * secondig
+        elif operator == "/":
+            if secondig == 0:
+                print("sorry, can't divide... :(")
+            else:
+                final = firstdig / secondig
+        else:
+            print("Unknown operator")
 
 
-print(final) 
+        print(f"result: {final}")
+
+    users_choice = input("Do you wish to continue?(Yes/No): ")
+    if users_choice is not ["yes", "y"]:
+        print("Thanks for using dorcas's calculator")
+        running = False
